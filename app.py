d44b999846edae3cf19e3059eae835fa190d6b28
koyamaryu123-ops <koyamaryu123@akane.waseda.jp>
2026-02-16 14:34:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if 'processed_files' not in st.session_state:
     st.session_state.processed_files = set()
 
 # サイドバーでAPIキー入力
-api_key = st.sidebar.text_input("AIzaSyDnSJrNQKia2ZHpKPXPnSdNuZlAVlqcvvQ", type="password")
+api_key = st.sidebar.text_input("AIzaSyDCRsVPD7krj2iYrwrogh37RCsplx8S5lc", type="password")
 if api_key:
     genai.configure(api_key=api_key)
 
